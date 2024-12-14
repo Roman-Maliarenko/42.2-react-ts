25b6464ad52f9d8740868d0399fd38eba28ff36a
Roman-Maliarenko <romamalarenko1@gmail.com>
2024-12-14 15:40:56 +0300

    change Lesson10

--- src/lessons/Lesson10/Lesson10.tsx
+++ src/lessons/Lesson10/Lesson10.tsx
@@ -1,57 +1,61 @@
 import {
-    Lesson10Container,
-    DataContainer,
-    ButtonContainer,
-    AllDataContainer,
-    Container,
-  } from "./styles";
-  import { useState } from "react";
-  import Button from "components/Button/Button";
-  
-  function Lesson10() {
-    const [data, setData] = useState<string[]>([]);
-    const [visible, setVisible] = useState<boolean>(false);
-  
-    const onClickData = async () => {
-      const CAT_FACTS = "https://catfact.ninja/fact";
-      try {
-        const response = await fetch(CAT_FACTS);
-        if (!response.ok) {
-          throw new Error("Error");
-        }
-        const result = await response.json();
-        setData((prevData) => updataData(prevData, [result.fact]));
-        setVisible(true);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    const onDeleteData = () => {
-      setData([]);
-      setVisible(false);
-    };
-    const updataData = (oldData: string[], newData: string[]) => {
-      return [...oldData, ...newData];
-    };
-  
-    return (
-      <Lesson10Container>
-        <ButtonContainer>
-          <Button name="GET MORE INFO" type="button" onClick={onClickData} />
-        </ButtonContainer>
-      <AllDataContainer>{visible}
-          <DataContainer>
-            {visible && data.map((fact) => <Container>{fact}</Container>)}
-          </DataContainer>
-        </AllDataContainer>
-        <ButtonContainer>
-          {visible && (
-            <Button name="DELETE ALL DATA" type="button" onClick={onDeleteData} />
-          )}
-        </ButtonContainer>
-      </Lesson10Container>
-    );
-  }
-  
-  export default Lesson10;
+  Lesson10Container,
+  DataContainer,
+  ButtonContainer,
+  AllDataContainer,
+  Container,
+} from "./styles";
+import { useState } from "react";
+import Button from "components/Button/Button";
+import axios from "axios";
+
+function Lesson10() {
+
+  const updataData = (oldData: string[], newData: string[]) => {
+    return [...oldData, ...newData];
+  };
+
+  const [data, setData] = useState<string[]>([]);
+  const [visible, setVisible] = useState<boolean>(false);
+  const [errors, setError] = useState<string>("")
+
+  const onClickData = async () => {
+    const CAT_FACTS = "https://catfact.ninja/fact";
+    try {
+      setError("Error");
+      const response = await axios.get(CAT_FACTS);
+      setData((prevData) => updataData(prevData, [response.data.fact]));
+      setVisible(true);
+    } catch (error) {
+      setError(errors)
+    }
+  };
+
+  const onDeleteData = () => {
+    setData([]);
+    setVisible(false);
+    setError("Error")
+  };
+
+  const renderData = data.map((fact, index) => (
+    <Container key={index}>{fact}</Container>
+  ));
+
+  return (
+    <Lesson10Container>
+      <ButtonContainer>
+        <Button name="GET MORE INFO" type="button" onClick={onClickData} />
+      </ButtonContainer>
+      <AllDataContainer>
+        {visible && <DataContainer>{renderData}</DataContainer>}
+      </AllDataContainer>
+      <ButtonContainer>
+        {visible && (
+          <Button name="DELETE ALL DATA" type="button" onClick={onDeleteData} />
+        )}
+      </ButtonContainer>
+    </Lesson10Container>
+  );
+}
+
+export default Lesson10;
